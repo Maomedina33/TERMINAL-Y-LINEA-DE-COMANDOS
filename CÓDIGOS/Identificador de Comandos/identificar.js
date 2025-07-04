809f55478b8e176c56b30be0ab21f9bf9096bec7
Maomedina33 <ingmaomedina@gmail.com>
2025-07-04 11:55:40 -0500

    Add files via upload

---
+++ CÓDIGOS/Identificador de Comandos/identificar.js
@@ -0,0 +1,12 @@
+type ls // identificador comando normal
+
+//Si deseas encontrar la ruta precisa del comando original que estás usando, 
+// la instrucción which será fundamental:
+which ls
+
+whereis ls  // este comando da un panorama mas amplio
+
+whatis grep //informacion rapida sobre que funcion cumple un comando
+
+ls /usr/bin/     //busqueda lista de binarios
+
